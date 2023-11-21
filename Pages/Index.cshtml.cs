2a284a354e420a5c029b1478c95dc6ac8a981ea4
Tamas Csabina <Tamas.Csabina@ext.esa.int>
2023-11-21 19:54:19 +0100

    First version with something shown on the page from the API

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,22 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Redmine.Net.Api;
+using Redmine.Net.Api.Types;
+using System.Collections.Specialized;
 
 namespace WebApplication1.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
+        public List<string>? IssueDetails { get; set; }
 
         public void OnGet()
         {
             string host = "http://127.0.0.1/";
             string apiKey = "8c2c0c5e67d6c424d6aa02ead559392d7570d2d7";
+            IssueDetails = new List<string>();
 
+            var manager = new RedmineManager(host, apiKey);
+
+            var parameters = new NameValueCollection { { RedmineKeys.STATUS_ID, RedmineKeys.ALL } };
+
+            foreach (var issue in manager.GetObjects<Issue>(parameters))
+            {
+                IssueDetails.Add($"#{issue.Id} | {issue.Status.Name} - {issue.Subject}");
+            }
         }
     }
 }
