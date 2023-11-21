1f0d86fb5f769179cfb88b9255bf2714709b8ddf
Tamas Csabina <Tamas.Csabina@ext.esa.int>
2023-11-21 18:40:00 +0100

    Initial comit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            string host = "http://127.0.0.1/";
+            string apiKey = "8c2c0c5e67d6c424d6aa02ead559392d7570d2d7";
+
+        }
+    }
+}
